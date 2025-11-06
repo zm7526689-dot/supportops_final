37231f12de196a966ae8c25cbe57c30a8f39c13b
zm7526689-dot <zm7526689@gmail.com>
2025-11-06 23:26:55 +0300

    Update create.php

--- views/customers/create.php
+++ views/customers/create.php
@@ -1,11 +1,22 @@
 <?php ob_start(); $cfg = require __DIR__ . '/../../config/config.php'; ?>
 <h1>إضافة عميل</h1>
-<?php if(!empty($error)): ?><p class="error"><?= Utils::h($error) ?></p><?php endif; ?>
+
+<?php if(!empty($error)): ?>
+  <p class="error"><?= Utils::h($error) ?></p>
+<?php endif; ?>
+
 <form method="post" action="<?= $cfg['APP_URL'] ?>/customers/create">
   <input type="hidden" name="csrf" value="<?= Utils::csrfToken() ?>">
-  <label>الاسم</label><input class="input" name="name" required>
-  <label>الهاتف</label><input class="input" name="phone" required>
-  <label>المنطقة</label><input class="input" name="area">
+  <label>الاسم</label>
+  <input class="input" name="name" required>
+
+  <label>الهاتف</label>
+  <input class="input" name="phone" required>
+
+  <label>المنطقة</label>
+  <input class="input" name="area">
+
   <button class="btn" type="submit">حفظ</button>
 </form>
+
 <?php $content = ob_get_clean(); include __DIR__ . '/../layout.php'; ?>
